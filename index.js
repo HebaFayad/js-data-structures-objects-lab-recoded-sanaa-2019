fb28a3d2f4ecfb2cca6b395852bb2b1450ee2fc9
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-12-13 08:07:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution in this file!
+let driver={};
+
+function updateDriverWithKeyAndValue(driver,key,value){
+  let newDri;
+ const newDri = {...driver};
+    newDri[key]=value;
+    return newDri;
+
+}
+
